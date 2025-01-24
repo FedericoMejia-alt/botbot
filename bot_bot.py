359a2db6d62a4f9c3b0a524d3b97cf549b77e385
federico mejia <mejiacarrillofederico@gmail.com>
2025-01-24 17:14:58 -0600

    Add files via upload

---
+++ bot_bot.py
@@ -0,0 +1,25 @@
+import discord
+from bot_logic import gen_pass
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'entramos como: {bot.user}')
+
+@bot.command()
+async def gen(ctx):
+    await ctx.send(f"Tu contraseña generada es: {gen_pass(10)}")
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+bot.run("MTMyOTk0MjgzODcyNDc4ODMyNw.GgaEBk.BDci8r_63KQeyb7E-9Q5JvMRcoo7BKxPc-fIc8")
